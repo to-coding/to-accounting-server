0be7d711af83fb18fa7a5a38bdddcb5d76ace45e
gaoxiang_huang <gaoxiang.huang@thoughtworks.com>
2023-12-15 09:54:41 +0800

    feat: allow CORS

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const data = require('./_db/data.json');
+
+// allow CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 
 app.get('/home', (req, res) => {
   res.send('Hello, World!')
+})
+
+app.get('/records', (req, res) => {
+  res.send(data.records)
 })
 
 app.listen(port, () => {
